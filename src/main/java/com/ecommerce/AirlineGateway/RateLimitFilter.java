4da07cd8f7df8092629d320817e0dff38aba8a30
Egemen Uner <23070006051@stu.yasar.edu.tr>
2026-04-02 00:43:21 +0300

    fix:sync

--- src/main/java/com/ecommerce/AirlineGateway/RateLimitFilter.java
+++ src/main/java/com/ecommerce/AirlineGateway/RateLimitFilter.java
@@ -17,30 +17,37 @@ import java.util.concurrent.ConcurrentHashMap;
 @Component
 public class RateLimitFilter implements Filter {
 
-    // Her bağlanan kullanıcının IP adresine özel bir "Kova" oluşturuyoruz
     private final Map<String, Bucket> cache = new ConcurrentHashMap<>();
 
     private Bucket createNewBucket() {
-        // KURAL: 1 Dakika içinde en fazla 3 isteğe izin ver (Test etmesi kolay olsun diye)
+        // PDF'teki kurala göre aslında "günde 3" olması isteniyor ancak test için "1
+        // dakikada 3" yapıldı.
         Bandwidth limit = Bandwidth.classic(3, Refill.greedy(3, Duration.ofMinutes(1)));
         return Bucket.builder().addLimit(limit).build();
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
 
-        // İsteği atan kişinin IP adresini al
+        String path = req.getRequestURI();
+
+        // KURAL: Eğer gelen istek "Query Flight" (flights/search) DEĞİLSE, limite
+        // sokmadan direkt geçir.
+        if (!path.contains("/api/v1/flights/search") && !path.contains("/api/v1/flights/all")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        // İstek Query Flight servisine geldiyse kotayı kontrol et
         String ip = req.getRemoteAddr();
         Bucket bucket = cache.computeIfAbsent(ip, k -> createNewBucket());
 
-        // Kovadan 1 hak harca
         if (bucket.tryConsume(1)) {
-            // Başarılı! Hakkı var, Gateway'den geçip 8080'e gitmesine izin ver
             chain.doFilter(request, response);
         } else {
-            // Başarısız! Kotası doldu (4. isteği attı). 8080'e yollamadan kapıdan çevir!
             res.setStatus(HttpStatus.TOO_MANY_REQUESTS.value()); // 429 Hatası
             res.getWriter().write("Too many requests! Please wait 1 minute.");
         }
