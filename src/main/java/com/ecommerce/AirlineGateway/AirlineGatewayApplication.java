f57a9e109dbb670508f9003c5e05a69afc60a171
Egemen Uner <23070006051@stu.yasar.edu.tr>
2026-04-20 02:45:19 +0300

    feat: inject backend URL from configuration into gateway routes

--- src/main/java/com/ecommerce/AirlineGateway/AirlineGatewayApplication.java
+++ src/main/java/com/ecommerce/AirlineGateway/AirlineGatewayApplication.java
@@ -1,5 +1,6 @@
 package com.ecommerce.AirlineGateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,9 @@ import static org.springframework.web.servlet.function.RequestPredicates.path;
 @SpringBootApplication
 public class AirlineGatewayApplication {
 
+	@Value("${BACKEND_URL:http://localhost:8080}")
+	private String backendUrl;
+
 	public static void main(String[] args) {
 		SpringApplication.run(AirlineGatewayApplication.class, args);
 	}
@@ -23,7 +27,7 @@ public class AirlineGatewayApplication {
 	public RouterFunction<ServerResponse> gatewayRoutes() {
 		return route("airline-main-api")
 				.route(path("/**"), http())
-				.before(uri("http://localhost:8080"))
+				.before(uri(backendUrl))
 				.build();
 	}
 }
